67eb37a5abf9156a63753a0e3b745696fbabc4a0
josrojrom1 <josrojrom@alum.us.es>
2026-03-31 22:09:17 +0200

    PMO-3: [Integracion de Ionic]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { IonApp, IonContent, IonHeader, IonToolbar, IonTitle, IonInput, IonButton, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { invoke } from "@tauri-apps/api/core";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, IonApp, IonContent, IonHeader, IonToolbar, IonTitle, IonInput, IonButton, IonItem, IonLabel],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
