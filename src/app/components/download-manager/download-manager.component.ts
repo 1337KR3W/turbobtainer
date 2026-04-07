11ed3b33be1fa998ea1f76076d10f85e8bb58790
josrojrom1 <josrojrom@alum.us.es>
2026-04-08 00:00:03 +0200

    Cambio de nombre de las funciondes de descarga, eliminado patron on para suprimir warnings

--- src/app/components/download-manager/download-manager.component.ts
+++ src/app/components/download-manager/download-manager.component.ts
@@ -2,14 +2,14 @@ import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TauriService } from '../../services/tauri.service';
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonIcon, IonInput, IonItem, IonProgressBar, IonRow, IonSpinner, IonText, IonLabel, IonGrid, IonList, IonThumbnail, IonFooter } from '@ionic/angular/standalone';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonIcon, IonInput, IonItem, IonProgressBar, IonRow, IonSpinner, IonLabel, IonGrid, IonList, IonThumbnail, IonFooter } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-download-manager',
   standalone: true,
   imports: [CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
     IonItem, IonInput, IonButton, IonIcon, IonRow, IonCol,
-    IonSpinner, IonProgressBar, IonText, IonCardSubtitle, IonLabel, IonGrid, IonList, IonThumbnail, IonFooter],
+    IonSpinner, IonProgressBar, IonCardSubtitle, IonLabel, IonGrid, IonList, IonThumbnail, IonFooter],
   templateUrl: './download-manager.component.html',
   styleUrls: ['./download-manager.component.scss']
 })
@@ -18,7 +18,7 @@ export class DownloadManagerComponent {
 
   @Input() url: string = '';
   @Output() urlChange = new EventEmitter<string>();
-  @Output() onAnalizar = new EventEmitter<'audio' | 'video'>();
-  @Output() onDownload = new EventEmitter<void>();
-  @Output() onCancel = new EventEmitter<void>();
+  @Output() analizar = new EventEmitter<'audio' | 'video'>();
+  @Output() download = new EventEmitter<void>();
+  @Output() cancelDld = new EventEmitter<void>();
 }
